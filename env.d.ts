0e678d3db577f223e66009a79e56ebdffd16b118
zfc <17602143402@163.com>
2024-02-16 13:05:45 +0800

    feature(custom): 降低threejs 版本以解决点光源失效的问题，GUI库引入

---
+++ env.d.ts
@@ -0,0 +1,3 @@
+// 解决引入模块的报错提示
+// 记得重启编辑器才生效
+declare module "three/addons/libs/lil-gui.module.min.js";
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,132 +5,107 @@
 import { onMounted } from 'vue';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-/*
- * 创建3D场景对象Scene
- */
+// 创建3D场景对象Scene
 const scene = new THREE.Scene();
 
-/*
- * 创建网格模型
- */
-
-// 定义几何体 长方形 BoxGeometry 长宽高都是50
-const geometry = new THREE.BoxGeometry(50, 50, 50);
-
-// 创建一个材质对象
-// 一种非光泽表面的材质 MeshLambertMaterial，没有镜面高光。
-// 该材质使用基于非物理的 Lambertian模型来计算反射率。
-const material = new THREE.MeshLambertMaterial({
-    color: '#f66' // 设置材质颜色
+const geometry = new THREE.BoxGeometry(30, 30, 30); 
+const material = new THREE.MeshPhongMaterial({ 
+    color: '#f66',
+    // transparent: true,
+    // opacity: 0.9,
 });
 
-// 使用 几何体 和 材质对象 创建一个网格模型，表示生活中的一个物体
-const mesh = new THREE.Mesh(geometry, material);
+// 创建网格模型，参数是几何体和材质对象，表示生活中的一个物体
+const mesh = new THREE.Mesh(geometry, material); 
+mesh.position.set(0, 0, 0); 
+scene.add(mesh); 
 
-// 设置网格模型在三维空间中的位置坐标，默认是坐标原点
-mesh.position.set(0, 0, 0);
-// 把网格模型添加到场景中
-scene.add(mesh);
-
-/*
- * 创建辅助坐标轴
- */
-
-//  AxesHelper 用于简单模拟3个坐标轴的对象.
-// 红色代表 X 轴. 绿色代表 Y 轴. 蓝色代表 Z 轴.
-// 参数 200 表示代表轴的线段长度. 默认为 1.
-const axesHelper = new THREE.AxesHelper(200);
-// 把坐标轴添加到场景中
-scene.add(axesHelper);
-
-/*
- * 添加光源
- */
-
-// 环境光 AmbientLight 会均匀的照亮场景中的所有物体，但是不能用来投射阴影，因为它没有方向。
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+// 添加光源 
+const ambientLight = new THREE.AmbientLight(0xffffff, 1); 
 scene.add(ambientLight);
+ 
+const pointLight = new THREE.PointLight(0xff0000, 1); 
+pointLight.position.set(60, 0, 0);
 
-// 点光源 从一个点向各个方向发射的光源。一个常见的例子是模拟一个灯泡发出的光。
-// color:灯光颜色，intensity:光照强度，默认为1.
-const pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.set(200, 300, 400);
 scene.add(pointLight);
 
-// 可视化点光源，创建一个虚拟的球形网格 Mesh 的辅助对象来模拟 点光源 PointLight.
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 20, 0xff0000);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 4, 0xff0000);
 scene.add(pointLightHelper);
 
-/*
- * 创建透视投影相机
- */
-
-// width和height用来设置渲染后，输出的画布宽高度。
+// 创建透视投影相机
 const width = window.innerWidth - 296;
 const height = window.innerHeight - 136;
+const camera = new THREE.PerspectiveCamera(30, width / height, 1, 8000); 
+camera.position.set(200, 200, 200); 
+camera.lookAt(0, 0, 0); 
 
-/*
- * 透视投影相机设置
- * 这一投影模式被用来模拟人眼所看到的景象，它是3D场景的渲染中使用得最普遍的投影模式。
- */
-// 30:视场角度, width / height:Canvas画布宽高比, 1:近裁截面, 3000：远裁截面
-const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
-//相机在Three.js三维坐标系中的位置
-camera.position.set(200, 200, 200);
-//相机观察目标指向Three.js坐标系原点
-camera.lookAt(0, 0, 0);
-
-/*
- * 创建3D渲染器
- */
-
-const renderer = new THREE.WebGLRenderer();
-//设置three.js渲染区域的尺寸(像素px)
+// 创建3D渲染器
+const renderer = new THREE.WebGLRenderer({
+    antialias: true, 
+});
 renderer.setSize(width, height);
-//执行渲染操作
-renderer.render(scene, camera);
-// Orbit controls（轨道控制器）可以使得相机围绕目标进行轨道运动。
-//创建控件对象
+renderer.render(scene, camera); 
+
+// 创建辅助坐标轴
+const axesHelper = new THREE.AxesHelper(80);
+scene.add(axesHelper);
+
+// 创建轨道控制器 OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // 监听鼠标，键盘事件
-// 如果使用了渲染循环（动画）renderFn（），就没有必要更新了change事件了；
 controls.addEventListener('change', () => {
     renderer.render(scene, camera);
 });
 
-/*
- * 动画，周期性执行，默认理想状态下是每秒钟执行60次
- * 渲染循环
- */
-
+// 动画
 const renderFn = () => {
-    // 周期性旋转，每次旋转0.01弧度
-    mesh.rotateY(0.06);
-    // 周期性执行相机的渲染功能，更新canvas画布上的内容
-    renderer.render(scene, camera);
+    // mesh.rotateY(0.06);  
+    renderer.render(scene, camera); 
     requestAnimationFrame(renderFn);
 };
 
 renderFn();
 
+// Gui 控制三维场景的UI交互界面
+const gui = new GUI();
+const obj = {
+    x: 0,
+    y: 0,
+    z: 0,
+    color: 0x00ffff,
+};
+gui.add(ambientLight, 'intensity', 0, 2).name('环境光强度').step(0.1);
+gui.add(pointLight, 'intensity', 0, 2).name('点光源强度').step(0.1);
+
+gui.add(obj, 'x', 0, 180).onChange((value) => {
+    mesh.position.x = value;
+});
+gui.add(obj, 'y', 0, 180).onChange((value) => {
+    mesh.position.y = value;
+});
+gui.add(obj, 'z', 0, 180).onChange((value) => {
+    mesh.position.z = value;
+});
+
+// 生成颜色值改变的交互界面
+gui.addColor(obj, 'color').onChange(function(value){
+    mesh.material.color.set(value);
+});
+
 // resize 事件会在窗口被调整大小时发生
 window.addEventListener('resize', () => {
     const w = window.innerWidth - 296;
     const h = window.innerHeight - 136;
-    // 重置渲染器输出画布canvas尺寸
     renderer.setSize(w, h);
-    // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
     camera.aspect = w / h;
-    // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
-    // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
-    // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
+    // 如果相机的一些属性发生了变化，需要执行 updateProjectionMatrix ()方法更新相机的投影矩阵
     camera.updateProjectionMatrix();
 });
 
 onMounted(() => {
-    //three.js执行渲染命令会输出一个canvas画布，也就是一个HTML元素，你可以插入到web页面中
     document.getElementById('container')?.appendChild(renderer.domElement);
 });
 </script>
