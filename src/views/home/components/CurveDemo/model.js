656732433387c53e53fab987fd875a9f8fdab32f
zfc <17602143402@163.com>
2024-03-30 13:11:21 +0800

    feature(custom): 样条，贝塞尔曲线应用

--- src/views/home/components/CurveDemo/model.js
+++ src/views/home/components/CurveDemo/model.js
@@ -1,21 +1,37 @@
 import * as THREE from 'three';
 
-// p1、p2、p3、p4表示4个点坐标
-// p1、p4是曲线起始点，p2、p3是曲线的控制点
-// const p1 = new THREE.Vector2(-80, 0);
-// const p2 = new THREE.Vector2(-40, 50);
-// const p3 = new THREE.Vector2(50, 50);
-// const p4 = new THREE.Vector2(80, 0);
+// p1、p3轨迹线起始点坐标
+// const p1 = new THREE.Vector3(-100, 0, -100);
+// const p3 = new THREE.Vector3(100, 0, 100);
 
-// 二维三次贝赛尔曲线
-// const curve = new THREE.CubicBezierCurve(p1, p2, p3, p4);
+// // 计算p1和p3的中点坐标
+// const x2 = (p1.x + p3.x) / 2;
 
-const p1 = new THREE.Vector3(-80, 0, 0);
-const p2 = new THREE.Vector3(-40, 50, 0);
-const p3 = new THREE.Vector3(50, 50, 0);
-const p4 = new THREE.Vector3(80, 0, 100);
-// 三维三次贝赛尔曲线
-const curve = new THREE.CubicBezierCurve3(p1, p2, p3, p4);
+// const z2 = (p1.z + p3.z) / 2;
+// const h = 50;
+// const p2 = new THREE.Vector3(x2, h, z2);
+
+// const arr = [p1, p2, p3];
+// // 三维样条曲线
+// const curve = new THREE.CatmullRomCurve3(arr);
+
+// p1、p3轨迹线起始点坐标
+const p1 = new THREE.Vector3(-100, 0, -100);
+const p3 = new THREE.Vector3(100, 0, 100);
+// 计算p1和p3的中点坐标
+const x2 = (p1.x + p3.x) / 2;
+const z2 = (p1.z + p3.z) / 2;
+// 三维样条曲线高度
+// const h = 50;
+// 三维二次贝赛尔曲线高度
+const h = 100;
+const p2 = new THREE.Vector3(x2, h, z2);
+
+const arr = [p1, p2, p3];
+// 三维样条曲线
+// const curve = new THREE.CatmullRomCurve3(arr);
+// 三维二次贝赛尔曲线
+const curve = new THREE.QuadraticBezierCurve3(...arr);
 
 // 获取曲线上的坐标点数量
 const pointArr = curve.getPoints(50);
@@ -31,7 +47,7 @@ const line = new THREE.Line(geometry, material);
 
 // 用点模型可视化贝塞尔曲线经过的顶点位置
 const geometryPoints = new THREE.BufferGeometry();
-geometryPoints.setFromPoints([p1, p2, p3, p4]);
+geometryPoints.setFromPoints([p1, p2, p3]);
 
 const materialPoints = new THREE.PointsMaterial({
     color: 0xff0000,
