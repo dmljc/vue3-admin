8958a0e817b4d424c8f7e14533cf8ff98b81bbfe
zfc <17602143402@163.com>
2024-03-30 22:53:54 +0800

    feature(custom): 旋转成型LatheGeometry

--- src/views/home/components/CurveDemo/model.js
+++ src/views/home/components/CurveDemo/model.js
@@ -1,11 +1,7 @@
 import * as THREE from 'three';
 
 // Vector2 表示的三个点坐标
-const pointsArr = [
-    new THREE.Vector2(50, 60), 
-    new THREE.Vector2(25, 0), 
-    new THREE.Vector2(50, -60),
-];
+const pointsArr = [new THREE.Vector2(50, 60), new THREE.Vector2(25, 0), new THREE.Vector2(50, -60)];
 // 通过三点定一个二维样条曲线
 const curves = new THREE.SplineCurve(pointsArr);
 // 曲线上获取点，作为旋转几何体的旋转轮廓
