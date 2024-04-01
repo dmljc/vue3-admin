f47938b439d7606601beddf2a948f4cf55288736
zfc <17602143402@163.com>
2024-04-01 18:38:32 +0800

    feature(custom): 多边形轮廓 Shape (圆弧)

--- src/views/home/components/CurveDemo/model.js
+++ src/views/home/components/CurveDemo/model.js
@@ -2,22 +2,27 @@ import * as THREE from 'three';
 
 const shape = new THREE.Shape();
 
-shape.moveTo(10, 0);
+// shape.lineTo(150, 0);
+// shape.arc(-50, 0, 50, 0, Math.PI / 2);
+
 shape.lineTo(100, 0);
-shape.lineTo(100, 100);
-shape.lineTo(10, 100);
+// shape.lineTo(150, 0);
 
-// ShapeGeometry 填充 Shape 获得一个平面几何体
-// const geometry = new THREE.ShapeGeometry(shape);
+// shape.arc(0, 0, 50, 0, Math.PI / 2, false);
+// shape.absarc(100, 0, 50, 0, Math.PI / 2, false);
+shape.absarc(100, 0, 50, 0, Math.PI / 2, false);
+shape.lineTo(0, 50);
 
-// ExtrudeGeometry 拉伸 Shape 获得一个长方体几何体
+// 填充
+// const geometry = new THREE.ShapeGeometry(shape, 20);
 const geometry = new THREE.ExtrudeGeometry(shape, {
-    depth: 40 //拉伸长度
+    depth: 40,
+    curveSegments: 20 // shape曲线对应曲线细分数
 });
+
 const materal = new THREE.MeshLambertMaterial({
     color: 0x00ffff
 });
 
 const mesh = new THREE.Mesh(geometry, materal);
-
 export default mesh;
---
+++ src/views/home/components/CurveDemo/model14.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+const shape = new THREE.Shape();
+
+shape.moveTo(10, 0);
+shape.lineTo(100, 0);
+shape.lineTo(100, 100);
+shape.lineTo(10, 100);
+
+// ShapeGeometry 填充 Shape 获得一个平面几何体
+// const geometry = new THREE.ShapeGeometry(shape);
+
+// ExtrudeGeometry 拉伸 Shape 获得一个长方体几何体
+const geometry = new THREE.ExtrudeGeometry(shape, {
+    depth: 40 //拉伸长度
+});
+const materal = new THREE.MeshLambertMaterial({
+    color: 0x00ffff
+});
+
+const mesh = new THREE.Mesh(geometry, materal);
+
+export default mesh;
