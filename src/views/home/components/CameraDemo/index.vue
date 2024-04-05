7815b5e73a2a51b8f948b1bb8f05bf9110a03707
zfc <17602143402@163.com>
2024-04-05 15:26:48 +0800

    feature(custom): 相机动画(.position和.lookAt())

--- src/views/home/components/CameraDemo/index.vue
+++ src/views/home/components/CameraDemo/index.vue
@@ -28,24 +28,24 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambientLight);
 
 // 透视投影相机
-// const width = window.innerWidth - 296;
-// const height = window.innerHeight - 136;
-// const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
-// camera.position.set(40, 122, 390);
-// camera.lookAt(0, 0, 0);
-
-// 正投影相机
 const width = window.innerWidth - 296;
 const height = window.innerHeight - 136;
-const k = width / height;
-// 根据包围盒大小调整s，包围盒y方向 尺寸的一半左右
-const s = 4;
-const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 8000);
-// 与观察点x、y一样，地图平行与canvas画布
-const x = 113.51,
-    y = 33.88;
-camera.position.set(x, y, 300);
-camera.lookAt(x, y, 0);
+const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
+camera.position.set(40, 122, 390);
+camera.lookAt(0, 0, 0);
+
+// 正投影相机
+// const width = window.innerWidth - 296;
+// const height = window.innerHeight - 136;
+// const k = width / height;
+// // 根据包围盒大小调整s，包围盒y方向 尺寸的一半左右
+// const s = 4;
+// const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 8000);
+// // 与观察点x、y一样，地图平行与canvas画布
+// const x = 113.51,
+//     y = 33.88;
+// camera.position.set(x, y, 300);
+// camera.lookAt(x, y, 0);
 
 // 渲染器
 const renderer = new THREE.WebGLRenderer({
@@ -55,8 +55,8 @@ renderer.setSize(width, height);
 
 // 相机轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(x, y, 0); // 与lookAt参数保持一致
-controls.update(); // update()函数内会执行camera.lookAt(controls.target)
+// controls.target.set(x, y, 0); // 与lookAt参数保持一致
+// controls.update(); // update()函数内会执行camera.lookAt(controls.target)
 
 controls.addEventListener('change', () => {
     renderer.render(scene, camera);
@@ -68,11 +68,12 @@ window.addEventListener('resize', () => {
     const height = window.innerHeight - 136;
     // 1. WebGL渲染器渲染的Cnavas画布尺寸更新
     renderer.setSize(width, height);
+    camera.aspect = width / height;
     // 2.1.更新相机参数
     // canvas画布宽高比会影响left, right需要跟着更新
-    const k = width / height; //canvas画布宽高比
-    camera.left = -s * k;
-    camera.right = s * k;
+    // const k = width / height; //canvas画布宽高比
+    // camera.left = -s * k;
+    // camera.right = s * k;
     // 2.2.相机的left, right等属性变化了，通知threejs系统
     camera.updateProjectionMatrix();
 });
@@ -87,7 +88,17 @@ onUnmounted(() => {
 });
 
 // 渲染循环
+let angle = 0;
+let R = 100;
 const render = () => {
+    // 相机圆周运动
+    angle += 0.01;
+    camera.position.x = R * Math.cos(angle);
+    camera.position.z = R * Math.sin(angle);
+    // 相机直线运动动画
+    // camera.position.z -= 0.2;
+
+    camera.lookAt(0, 0, 0);
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 };
