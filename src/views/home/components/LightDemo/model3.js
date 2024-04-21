381245032c8b52e25d912a2f5f4f7b7e73d22b7c
zfc <17602143402@163.com>
2024-04-21 12:12:30 +0800

    feature(custom): gui 辅助调节光源阴影

---
+++ src/views/home/components/LightDemo/model3.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+// 立方体
+const geometry = new THREE.BoxGeometry(50, 100, 50);
+const material = new THREE.MeshLambertMaterial({
+    color: 0x00ffff,
+    side: THREE.DoubleSide
+});
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.y = 50;
+// 1、设置产生投影的网格模型
+mesh.castShadow = true;
+
+// 平面
+const planeGeometry = new THREE.PlaneGeometry(400, 850);
+const planeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x999999,
+    side: THREE.DoubleSide
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.rotateX(-Math.PI / 2);
+// 3、设置接收阴影的投影面
+planeMesh.receiveShadow = true;
+
+const group = new THREE.Group();
+group.add(mesh, planeMesh);
+
+for (let i = -3; i < 4; i++) {
+    const mesh2 = mesh.clone();
+    // 设置产生投影的网格模型
+    mesh2.castShadow = true;
+    mesh.position.z = i * 100;
+    group.add(mesh2);
+}
+
+export default group;
