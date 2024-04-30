5f25ffda2ccf923c0fa8acf701ab37e8be9b44ed
zfc <17602143402@163.com>
2024-04-30 13:11:34 +0800

    feature(custom): 射线

---
+++ src/views/home/components/RayCaster/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div ref="raycaster"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed,onMounted, onUnmounted } from 'vue';
+import * as THREE from 'three';
+import model from './model.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const raycaster = ref(null);
+
+const width = computed(() => {
+    return  window.innerWidth - 174;
+});
+const height = computed(() => {
+    return  window.innerHeight - 136;
+});
+// 场景
+const scene = new THREE.Scene();
+
+scene.add(model);
+
+// 辅助观察坐标系
+const axesHelper = new THREE.AxesHelper(200);
+scene.add(axesHelper);
+
+// 灯光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(100, 60, 50);
+scene.add(directionalLight);
+
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
+
+// 相机
+// const width = window.innerWidth - 296;
+// const height = window.innerHeight - 136;
+const camera = new THREE.PerspectiveCamera(30, width.value / height.value, 1, 3000);
+camera.position.set(350, 250, 300);
+camera.lookAt(0, 0, 0);
+
+// 渲染器
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.setPixelRatio(window.devicePixelRatio); //防止输出模糊
+renderer.setSize(width.value, height.value);
+
+// 相机轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', () => {
+    renderer.render(scene, camera);
+});
+
+// resize 事件会在窗口被调整大小时发生
+window.addEventListener('resize', () => {
+    // const w = window.innerWidth - 296;
+    // const h = window.innerHeight - 136;
+    renderer.setSize(width.value, height.value);
+    camera.aspect = width.value / height.value;
+    // 如果相机的一些属性发生了变化，需要执行 updateProjectionMatrix ()方法更新相机的投影矩阵
+    camera.updateProjectionMatrix();
+});
+
+onMounted(() => {
+    renderer.setClearColor(0x444544, 0.4);
+    raycaster.value?.appendChild(renderer.domElement);
+});
+
+onUnmounted(() => {
+    renderer.dispose();
+});
+
+// 渲染循环
+const render = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+};
+render();
+</script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,7 @@
     <!-- <Vector3 /> -->
     <!-- <dome /> -->
     <!-- <uv-texture /> -->
-    <blender />
+    <!-- <blender /> -->
     <!-- <PBRMaterial /> -->
     <!-- <canvas-event /> -->
     <!-- <curve-demo /> -->
@@ -11,7 +11,8 @@
     <!-- <orbit-controls /> -->
     <!-- <light-demo /> -->
     <!-- <sprite-demo /> -->
-    <effect-composer />
+    <!-- <effect-composer /> -->
+    <ray-caster />
 </template>
 
 <script setup lang="ts">
@@ -19,7 +20,7 @@
 // import Vector3 from './components/Vector3/index.vue';
 // import Dome from './components/Demo/index.vue';
 // import UvTexture from './components/UvTexture/index.vue';
-import Blender from './components/Blender/index.vue';
+// import Blender from './components/Blender/index.vue';
 // import PBRMaterial from './components/PBRMaterial/index.vue';
 // import CanvasEvent from './components/CanvasEvent/index.vue';
 // import CurveDemo from './components/CurveDemo/index.vue';
@@ -27,5 +28,6 @@ import Blender from './components/Blender/index.vue';
 // import OrbitControls from './components/OrbitControls/index.vue';
 // import LightDemo from './components/LightDemo/index.vue';
 // import SpriteDemo from './components/SpriteDemo/index.vue';
-import EffectComposer from './components/EffectComposer/index.vue';
+// import EffectComposer from './components/EffectComposer/index.vue';
+import RayCaster from './components/RayCaster/index.vue';
 </script>
